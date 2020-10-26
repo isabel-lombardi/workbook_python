59e5eeac72de47a12ca4fabe0f997162dea2c0bf
Isabel Lombardi <isabel.lombardi96@gmail.com>
2020-10-26 16:18:27 +0100

    Update ex_138

---
+++ cap_6/ex_143.py
@@ -0,0 +1,25 @@
+# Anagrams
+
+def anagrams(s):
+    counts = {}
+    for ch in s:
+        if ch in counts:
+            counts[ch] += 1
+        else:
+            counts[ch] = 1
+    return counts
+
+
+def main():
+    s1 = input("Enter the first string: ")
+    s2 = input("Enter the second string: ")
+
+    counts1 = anagrams(s1)
+    counts2 = anagrams(s2)
+    if counts1 == counts2:
+        print("The strings are anagrams")
+    else:
+        print("The strings are not anagrams")
+
+
+main()
