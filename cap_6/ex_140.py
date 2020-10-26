c3b09c1dad02c3bedd6db9be9598256edff6b0df
Isabel Lombardi <isabel.lombardi96@gmail.com>
2020-10-26 16:48:41 +0100

    ex_140

--- cap_6/ex_140.py
+++ cap_6/ex_140.py
@@ -1,6 +1,6 @@
 # Postal Codes
 
-pos = {"Newfoundland": "A", "Nova Scotia": "B", "Prince Edward Island": "C", "New Brunswick": "E",
+postal = {"Newfoundland": "A", "Nova Scotia": "B", "Prince Edward Island": "C", "New Brunswick": "E",
              "Quebec": ["G", "H", "I"], "Ontario": ["K", "L", "M", "N", "P"], "Manitoba": "R", "Saskatchewan": "S",
              "Alberta": "T", "British Columbia": "V", "Nunavut": "X", "or Northwest Territories": "X", "Yukon": "Y"}
 
@@ -10,9 +10,9 @@ if postal_code[1] == 0:
     result += "a rural "
 else:
     result += "an urban "
-for k, v in pos.items():
-    if postal_code[0] in v:
-        result += "address " + k
+for key, value in postal.items():
+    if postal_code[0] in value:
+        result += "address " + key
 
 
 print("The postal code {} is for {} ".format(postal_code, result))
