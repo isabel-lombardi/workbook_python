7149fa91abb2efa510667b25217fbed6fb2d8b79
Isabel Lombardi <isabel.lombardi96@gmail.com>
2020-10-26 16:32:35 +0100

    ex_143

--- cap_6/ex_143.py
+++ cap_6/ex_143.py
@@ -11,7 +11,7 @@ def anagrams(s):
 
 
 def main():
-    s1 = input("Enter the first string: ")
+    s1 = input('Enter the first string: ')
     s2 = input("Enter the second string: ")
 
     counts1 = anagrams(s1)
