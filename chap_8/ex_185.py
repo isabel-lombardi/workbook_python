01923a3b27d9c0eab3a49ebfdf49d229ca2a8f33
Isabel Lombardi <isabel.lombardi96@gmail.com>
2020-11-05 20:09:19 +0100

    update

--- chap_8/ex_185.py
+++ chap_8/ex_185.py
@@ -1,23 +1,21 @@
 # Run-Length decoding
+from chap_8.ex_184 import flatten
+
 
 def decode(text):
     if not text:
-        return ""
+        return []
     else:
         char = text[0]
         quantity = text[1]
-        return char * int(quantity) + decode(text[2:])
+        return [char] * quantity + flatten(decode(text[2:]))
 
 
 def main():
     code = ['A', 12, 'B', 4, 'A', 6, 'B', 3]
     print("The compressed list is: {}".format(code))
-    print()
-    deco = []
-    for e in decode(code):
-        deco.append(e)
     print("The decompressed list is: \n"
-          "{}".format(deco))
+          "{}".format(decode(code)))
 
 
 if __name__ == "__main__":
